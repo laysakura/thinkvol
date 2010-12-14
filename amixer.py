8a17a655ec4fadf2b749dbbe74e5d385fe028952
Sho Nakatani <lay.sakura@gmail.com>
2010-12-15 04:24:58 +0900

    Mute/Unmute完成 Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- amixer.py
+++ amixer.py
@@ -1,4 +1,30 @@
+import os
+import popen2
+
 def togglemute():
+    def _ismute():        
+        cmd = 'amixer get Master'
+        (stdout, stdin, stderr) = popen2.popen3( cmd  )
+        for line in stdout:
+            if line.find('[on]') != -1:
+                return False
+            elif line.find('[off]') != -1:
+                return True
+
+    def _mute():
+        cmd = 'amixer set Master mute'
+        os.system(cmd)
+
+    def _unmute():
+        cmd = 'amixer set Master unmute'
+        os.system(cmd)
+
+    if _ismute():
+        _unmute()
+    else:
+        _mute()
+
+
     print('mute')
 
 def voldown():
