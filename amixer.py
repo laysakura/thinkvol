8a5006cafd4c93f2ef672821924583649168ad73
Sho Nakatani <lay.sakura@gmail.com>
2010-12-15 04:48:42 +0900

    動いた．READMEつけて完成かな． Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- amixer.py
+++ amixer.py
@@ -1,15 +1,20 @@
 import os
 import popen2
+import re
+
+amixer_volline = re.compile('^  Front (Left|Right): Playback [0-9]{1,3} \[([0-9]{1,3})%\] \[.+dB\] \[(on|off)\]$')
 
 def togglemute():
     def _ismute():        
         cmd = 'amixer get Master'
-        (stdout, stdin, stderr) = popen2.popen3( cmd  )
+        (stdout, stdin, stderr) = popen2.popen3(cmd)
         for line in stdout:
-            if line.find('[on]') != -1:
-                return False
-            elif line.find('[off]') != -1:
-                return True
+            m = amixer_volline.match(line)
+            if m is not None:
+                if m.group(3) == 'on':
+                    return False
+                elif m.group(3) == 'off':
+                    return True
 
     def _mute():
         cmd = 'amixer set Master mute'
@@ -24,11 +29,48 @@ def togglemute():
     else:
         _mute()
 
-
-    print('mute')
-
 def voldown():
-    print('down')
+    def _curvol():
+        cmd = 'amixer get Master'
+        (stdout, stdin, stderr) = popen2.popen3(cmd)
+        for line in stdout:
+            m = amixer_volline.match(line)
+            if m is not None:
+                return int(m.group(2))
+
+    def _newvol(curvol):
+        newvol = curvol - 3
+        if newvol < 0:
+            newvol = 0
+        return newvol
+
+    def _set(newvol):
+        cmd = 'amixer set Master ' + str(newvol) + '%'
+        os.system(cmd)
+
+    curvol = _curvol()
+    newvol = _newvol(curvol)
+    _set(newvol)
 
 def volup():
-    print('up')
+    def _curvol():
+        cmd = 'amixer get Master'
+        (stdout, stdin, stderr) = popen2.popen3(cmd)
+        for line in stdout:
+            m = amixer_volline.match(line)
+            if m is not None:
+                return int(m.group(2))
+
+    def _newvol(curvol):
+        newvol = curvol + 3
+        if newvol > 100:
+            newvol = 100
+        return newvol
+
+    def _set(newvol):
+        cmd = 'amixer set Master ' + str(newvol) + '%'
+        os.system(cmd)
+
+    curvol = _curvol()
+    newvol = _newvol(curvol)
+    _set(newvol)
